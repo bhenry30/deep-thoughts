1de3d7bc4582934f03ad3190701ec07a87bbcd87
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-08-08 12:00:49 -0500

    adds thought form and component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from '../components/ThoughtForm';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
 
@@ -12,9 +13,15 @@ const Home = () => {
   const { data: userData } = useQuery(QUERY_ME_BASIC);
   const thoughts = data?.thoughts || []
   const loggedIn = Auth.loggedIn();
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
+      {loggedIn && (
+      <div className="col-12 mb-3">
+        <ThoughtForm />
+      </div>
+    )}
       <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
   {loading ? (
     <div>Loading...</div>
